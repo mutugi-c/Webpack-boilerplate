b5fd9b9a0cc547638dac2e344f6e8cf2377842fa
mutugi-c <mutugichris@gmail.com>
2023-03-28 21:32:00 +0200

    Add css

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,12 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
